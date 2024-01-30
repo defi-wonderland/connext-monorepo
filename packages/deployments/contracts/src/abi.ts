bb6a20365c4a6d9236e560202882a12f1b0b3574
0xRaccoon <raccoon@defi.sucks>
2024-01-30 10:32:03 -0300

    refactor: move contracts folders

--- packages/deployments/contracts/src/abi.ts
+++ packages/deployments/contracts/src/abi.ts
@@ -1,10 +1,10 @@
-import _ConnextAbi from "../abi/contracts/core/connext/interfaces/IConnext.sol/IConnext.json";
-import _TokenFacetAbi from "../abi/contracts/core/connext/facets/TokenFacet.sol/TokenFacet.json";
-import _BridgeFacetAbi from "../abi/contracts/core/connext/facets/BridgeFacet.sol/BridgeFacet.json";
-import _InboxFacetAbi from "../abi/contracts/core/connext/facets/InboxFacet.sol/InboxFacet.json";
-import _AdminSpokeConnectorAbi from "../abi/contracts/messaging/connectors/admin/AdminSpokeConnector.sol/AdminSpokeConnector.json";
-import _AdminHubConnectorAbi from "../abi/contracts/messaging/connectors/admin/AdminHubConnector.sol/AdminHubConnector.json";
-import _RootManagerAbi from "../abi/contracts/messaging/RootManager.sol/RootManager.json";
+import _ConnextAbi from "../abi/contracts/solidity/core/connext/interfaces/IConnext.sol/IConnext.json";
+import _TokenFacetAbi from "../abi/contracts/solidity/core/connext/facets/TokenFacet.sol/TokenFacet.json";
+import _BridgeFacetAbi from "../abi/contracts/solidity/core/connext/facets/BridgeFacet.sol/BridgeFacet.json";
+import _InboxFacetAbi from "../abi/contracts/solidity/core/connext/facets/InboxFacet.sol/InboxFacet.json";
+import _AdminSpokeConnectorAbi from "../abi/contracts/solidity/messaging/connectors/admin/AdminSpokeConnector.sol/AdminSpokeConnector.json";
+import _AdminHubConnectorAbi from "../abi/contracts/solidity/messaging/connectors/admin/AdminHubConnector.sol/AdminHubConnector.json";
+import _RootManagerAbi from "../abi/contracts/solidity/messaging/RootManager.sol/RootManager.json";
 
 export const ConnextAbi = _ConnextAbi;
 export const TokenFacetAbi = _TokenFacetAbi;
