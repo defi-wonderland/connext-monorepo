e942e31fe94808cf1f7998add6d26424e5297787
0xJabberwock <0xjabberwock@defi.sucks>
2024-02-14 03:35:12 -0300

    feat: remove BaseConnext contract

--- packages/deployments/contracts/contracts/chimera/BaseConnext.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity 0.8.17;
-
-import {IBaseConnext} from './interfaces/IBaseConnext.sol';
-
-abstract contract BaseConnext is IBaseConnext {}
